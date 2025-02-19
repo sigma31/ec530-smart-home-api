4685e283d1a28db31c027d2d61c32ce7a4455211
sigma31 <pranetsh@bu.edu>
2025-02-18 23:45:36 -0500

    Forgot to add the password field in the DB

--- app/models/models.py
+++ app/models/models.py
@@ -6,6 +6,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
+    password_hash = db.Column(db.String(128), nullable=False)
 
 class House(db.Model):
     id = db.Column(db.Integer, primary_key=True)
